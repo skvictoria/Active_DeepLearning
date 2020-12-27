6f2ebcb959c5bd09eac75896d21aeadfb1b9b9b0
Seulgi Kim(Sophia) <40836553+skvictoria@users.noreply.github.com>
2020-12-27 13:35:55 +0900

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -24,10 +24,11 @@ from collections import namedtuple, OrderedDict
 flags = tf.app.flags
 flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
-flags.DEFINE_string('label', '', 'Name of class label')
+# flags.DEFINE_string('label', '', 'Name of class label')
 # if your image has more labels input them as
-# flags.DEFINE_string('label0', '', 'Name of class[0] label')
-# flags.DEFINE_string('label1', '', 'Name of class[1] label')
+flags.DEFINE_string('label0', '', 'Name of class[0] label')
+flags.DEFINE_string('label1', '', 'Name of class[1] label')
+flags.DEFINE_string('label2', '', 'Name of class[2] label')
 # and so on.
 flags.DEFINE_string('img_path', '', 'Path to images')
 FLAGS = flags.FLAGS
@@ -36,13 +37,15 @@ FLAGS = flags.FLAGS
 # TO-DO replace this with label map
 # for multiple labels add more else if statements
 def class_text_to_int(row_label):
-    if row_label == FLAGS.label:  # 'ship':
-        return 1
+    #if row_label == FLAGS.label:  # 'ship':
+    #    return 1
     # comment upper if statement and uncomment these statements for multiple labelling
-    # if row_label == FLAGS.label0:
-    #   return 1
-    # elif row_label == FLAGS.label1:
-    #   return 0
+    if row_label == FLAGS.label0:
+        return 2
+    elif row_label == FLAGS.label1:
+        return 1
+    elif row_label == FLAGS.label2:
+        return 0
     else:
         None
 
