3d3da1e2a63aabf093d66e0da56f49c69364901b
Seulgi Kim(Sophia) <40836553+skvictoria@users.noreply.github.com>
2020-12-29 22:08:04 +0900

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -22,15 +22,16 @@ from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
 flags = tf.app.flags
-flags.DEFINE_string('csv_input', '', '/home/john/project/repos/models/research/object_detection/csvlabeling')
-flags.DEFINE_string('output_path', '', '/home/john/project/repos/models/research/object_detection')
+flags.DEFINE_string('big_csv_input', '', '/home/john/project/repos/models/research/object_detection/images/csvlowbiglabeling')
+flags.DEFINE_string('low_csv_input', '', '/home/john/project/repos/models/research/object_detection/images/csvlowbiglabeling')
+flags.DEFINE_string('image_dir', '', '/home/john/project/repos/models/research/object_detection/images/train')
+flags.DEFINE_string('output_path', '', '/home/john/project/repos/models/research/object_detection/csvlabeling')
+
 # flags.DEFINE_string('label', '', 'Name of class label')
 # if your image has more labels input them as
 #flags.DEFINE_string('label0', '', 'Name of class[0] label')
 #flags.DEFINE_string('label1', '', 'Name of class[1] label')
 #flags.DEFINE_string('label2', '', 'Name of class[2] label')
-# and so on.
-flags.DEFINE_string('img_path', '', '/home/john/project/repos/models/research/object_detection/train')
 FLAGS = flags.FLAGS
 
 
@@ -46,8 +47,6 @@ def class_text_to_int(row_label):
         return 2
     elif row_label == 'scorch':
         return 3
-    elif row_label == 'normal':
-        return 4
     else:
         None
 
@@ -58,55 +57,66 @@ def split(df, group):
     return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
 
 
-def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
-        encoded_jpg = fid.read()
-    encoded_jpg_io = io.BytesIO(encoded_jpg)
-    image = Image.open(encoded_jpg_io)
-    width, height = image.size
+def create_tf_example(group, path, image_format):
+    try:
+        with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+            encoded_jpg = fid.read()
+        encoded_jpg_io = io.BytesIO(encoded_jpg)
+        image = Image.open(encoded_jpg_io)
+        width, height = image.size
 
-    filename = group.filename.encode('utf8')
-    image_format = (b'jpg' || b'JPG')
-    # check if the image format is matching with your images.
-    xmins = []
-    xmaxs = []
-    ymins = []
-    ymaxs = []
-    classes_text = []
-    classes = []
+        filename = group.filename.encode('utf8')
+        #image_format = (b'jpg' || b'JPG')
+        # check if the image format is matching with your images.
+        xmins = []
+        xmaxs = []
+        ymins = []
+        ymaxs = []
+        classes_text = []
+        classes = []
 
-    for index, row in group.object.iterrows():
-        xmins.append(row['xmin'] / width)
-        xmaxs.append(row['xmax'] / width)
-        ymins.append(row['ymin'] / height)
-        ymaxs.append(row['ymax'] / height)
-        classes_text.append(row['class'].encode('utf8'))
-        classes.append(class_text_to_int(row['class']))
+        for index, row in group.object.iterrows():
+            xmins.append(row['xmin'] / width)
+            xmaxs.append(row['xmax'] / width)
+            ymins.append(row['ymin'] / height)
+            ymaxs.append(row['ymax'] / height)
+            classes_text.append(row['class'].encode('utf8'))
+            classes.append(class_text_to_int(row['class']))
 
-    tf_example = tf.train.Example(features=tf.train.Features(feature={
-        'image/height': dataset_util.int64_feature(height),
-        'image/width': dataset_util.int64_feature(width),
-        'image/filename': dataset_util.bytes_feature(filename),
-        'image/source_id': dataset_util.bytes_feature(filename),
-        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
-        'image/format': dataset_util.bytes_feature(image_format),
-        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
-        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
-        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
-        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
-        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-        'image/object/class/label': dataset_util.int64_list_feature(classes),
-    }))
-    return tf_example
+        tf_example = tf.train.Example(features=tf.train.Features(feature={
+            'image/height': dataset_util.int64_feature(height),
+            'image/width': dataset_util.int64_feature(width),
+            'image/filename': dataset_util.bytes_feature(filename),
+            'image/source_id': dataset_util.bytes_feature(filename),
+            'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+            'image/format': dataset_util.bytes_feature(image_format),
+            'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+            'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+            'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+            'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+            'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+            'image/object/class/label': dataset_util.int64_list_feature(classes),
+        }))
+        return tf_example
+    except:
+        return 0
 
 
 def main(_):
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(os.getcwd(), FLAGS.img_path)
-    examples = pd.read_csv(FLAGS.csv_input)
+    examples = pd.read_csv(FLAGS.low_csv_input)
     grouped = split(examples, 'filename')
     for group in grouped:
-        tf_example = create_tf_example(group, path)
+        tf_example = create_tf_example(group, path, b'jpg')
+        if(tf_example==0):
+            continue
+        writer.write(tf_example.SerializeToString())
+        
+    examples = pd.read_csv(FLAGS.big_csv_input)
+    grouped = split(examples, 'filename')
+    for group in grouped:
+        tf_example = create_tf_example(group, path, b'JPG')
         writer.write(tf_example.SerializeToString())
 
     writer.close()
