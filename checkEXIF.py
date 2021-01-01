299a6a4e1d41b40af811b849170e466f620c7a6f
Seulgi Kim(Sophia) <40836553+skvictoria@users.noreply.github.com>
2021-01-01 19:00:38 +0900

    Update checkEXIF.py

--- checkEXIF.py
+++ checkEXIF.py
@@ -1,23 +1,32 @@
+import PIL
 from PIL import Image, ExifTags
+import os
+import cv2
 
-try:
-    image=Image.open(filepath)
 
-    for orientation in ExifTags.TAGS.keys():
-        if ExifTags.TAGS[orientation]=='Orientation':
-            break
-    
-    exif = image._getexif()
+for filename in os.listdir('./test'):
 
-    if exif[orientation] == 3:
-        image=image.rotate(180, expand=True)
-    elif exif[orientation] == 6:
-        image=image.rotate(270, expand=True)
-    elif exif[orientation] == 8:
-        image=image.rotate(90, expand=True)
+    try:
+        image = Image.open((os.path.join('./test', filename)))
 
-    image.save(filepath)
-    image.close()
-except (AttributeError, KeyError, IndexError):
-    # cases: image don't have getexif
-    pass
+        new_filename = os.path.join('./test', filename)
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] =='Orientation':
+                break
+
+        exif = image._getexif()
+
+        image = cv2.imread((os.path.join('./test', filename)), cv2.IMREAD_COLOR)
+
+        if exif[orientation]==3:
+            img_rotate = cv2.rotate(image, cv2.ROTATE_180)
+            cv2.imwrite(new_filename, img_rotate)
+        elif exif[orientation] == 6:
+            img_rotate = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            cv2.imwrite(new_filename, img_rotate)
+        elif exif[orientation] == 8:
+            img_rotate = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+            cv2.imwrite(new_filename, img_rotate)
+
+    except(PIL.UnidentifiedImageError, TypeError, AttributeError, KeyError, IndexError):
+        pass
